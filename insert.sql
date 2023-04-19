328cd10faa25a7ccefddd46b37a6999a3c900cd0
Yonas Chemeda <z1899807@turing.cs.niu.edu>
2023-04-19 14:52:05 -0500

    sql script for inserting data

---
+++ insert.sql
@@ -0,0 +1,64 @@
+INSERT INTO Customers 
+(Email,
+CustID,
+Name,
+Phone)  /* data set 1 */
+VALUES('robertdavis1234@gmail.com',
+        '1234',
+        'Robert Davis',
+        '555-123-4567'),
+        /* data set 2 */
+        (' sarahjohnson5678@yahoo.com',
+        '5678',
+        'Sarah Johnson',
+        '555-987-6543'),
+        /* data set 3 */
+        ('mikejackson9101@hotmail.com',
+        '9101',
+        'Mike Jackson',
+        '555-555-1212'),
+        /* data set 4 */
+        ('janetwilson1212@gmail.com',
+        '1212',
+        ' Janet Wilson',
+        '555-555-5555'),
+        /* data set 5 */
+        ('jasonsanchez3434@yahoo.com',
+        '3434',
+        'Jason Sanchez',
+        '555-555-1234'),
+        /* data set 6 */
+        ('karenrodriguez5555@hotmail.com',
+        '5555',
+        'Karen Johnson',
+        '555-555-4321'),
+        /* data set 7 */
+        ('davidbrown7890@gmail.com',
+        '7890',
+        'David Brown',
+        '555-555-9876'),
+        /* data set 8 */
+        ('amandamiller2468@yahoo.com',
+        '2468',
+        'Amanda Miller',
+        '555-555-3478'),
+        /* data set 9 */
+        ('kevinsullivan2222@hotmail.com',
+        '2222',
+        'Kevin Sullivan',
+        '555-555-2121'),
+        /* data set 10 */
+        ('lisajones7777@gmail.com',
+        '7777',
+        'Lisa Jones',
+        '773-301-4908');
+
+INSERT INTO Song
+(Title,
+Genre,
+SongID,
+Year,
+Version,
+Duration)
+VALUES()
+
