7576ba8aa0d6a7f92de67b044590266bef97b422
unknown <ecarbajal9221@gmail.com>
2023-04-21 11:42:09 -0500

    Added startup and creating table php files

---
+++ showTables.php
@@ -0,0 +1,39 @@
+<?php
+# CSCI 466 Project - showTables.php
+#  
+# A function that creates a table in html tags from a result set given by PDO.
+# Returns a boolean to inform the caller whether an empty set was found or not.
+# ===============================================
+function createTable($result_set)
+{
+    if($result_set) 
+    {
+        echo "<table border=1 cellspacing=1>\n";
+        $i = 0;
+ 
+        while($row = $result_set->fetch(PDO::FETCH_ASSOC))
+        {
+            #print headers of tables on top row
+            if($i == 0)
+            {
+                echo "<tr>\n";
+                foreach($row as $key => $item) { echo "<th>$key</th>"; }
+                echo "</tr>\n";
+            }
+            echo "<tr>\n";
+            foreach($row as $key => $item) { echo "<td>$item</td>"; }
+            echo "</tr>\n";
+            $i++;
+        }
+        echo "</table>\n"; 
+        if($i == 0)
+        {
+            echo "<p>Empty set! Nothing was shown.</p>\n";
+            return false; 
+        }
+    }
+    else{ echo "<p>Table not created - Failed to obtain a result set.</p>\n"; return false; }
+ 
+    return true;
+}
+?>
