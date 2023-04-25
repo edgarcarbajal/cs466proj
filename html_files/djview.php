e8f368ba5a48301572e8d1f4613592b06448791c
Yonas Chemeda <z1899807@turing.cs.niu.edu>
2023-04-25 16:43:45 -0500

    working on the dj view

--- html_files/djview.php
+++ html_files/djview.php
@@ -8,7 +8,36 @@
         <h1>Nothing here yet... (DJ View)</h1>
 
         <?php
+        
+        include "../php_files/PDOStartup.php";
+
+        // give the dj the abilty to see regualr Queue
+        echo "<h3>Queue</h3>";
+        $queue = $pdo->prepare("SELECT * from Queues order by Time");
+        $queue->execute();
+        createTable($queue);
+        
+        // give the dj the ability to see PriorityQueue
+        echo "<h3>PriorityQueue</h3>";
+        $Pqueue = $pdo->prepare("SELECT * from PriorityQueues order by Time");
+        $Pqueue->execute();
+        createTable($Pqueue);
+
+        // get first person from the queue and add it into where clause under
+        $queue2 = $pdo->prepare("SELECT Name,Time from Customers,PriorityQueues where Customers.CustID = PriorityQueues.CustID order by Time");
+        $queue2->execute();
+        $result = $queue2 -> fetch(PDO :: FETCH_ASSOC);
+        if($result)
+        {
+            echo "</br>";
+            echo "<h1>";
+            echo "Next to sing &nbsp" . $result['Name'];  
+            echo "</br>";
+            echo "</h1>";
+        }
+
         ?>
+      
 
         <a href="startpage.html">
             <input type="button" value="Go Back">
