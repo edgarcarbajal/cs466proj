8855fd6b804c9ec5ee4fc9072937efbb213dce8e
Yonas Chemeda <z1899807@turing.cs.niu.edu>
2023-04-26 15:26:27 -0500

    .

--- html_files/djview.php
+++ html_files/djview.php
@@ -15,19 +15,43 @@
         echo "<h3>Queue</h3>";
         $queue = $pdo->prepare("SELECT * from Queues order by Time");
         $queue->execute();
-        createTable($queue);
+        createTableRadio($queue);
         
         // give the dj the ability to see PriorityQueue
         echo "<h3>PriorityQueue</h3>";
+        echo "<form method = \"get\"";
         $Pqueue = $pdo->prepare("SELECT * from PriorityQueues order by Time");
         $Pqueue->execute();
-        createTable($Pqueue);
+       echo "<table border = 1 cellspacing = 1>
+       <thead>
+         <tr>
+         <h1>
+           <th>CustID &nbsp;</th>
+           <th>SongID &nbsp;</th>
+           <th>Version &nbsp;</th>
+           <th>Time &nbsp;</th>
+           <th>&nbsp; Money &nbsp;</th>
+           </h1>
+         </tr>
+       </thead>
+       <tbody>";
+        while($row = $Pqueue -> fetch())
+        {
+            $id = $row['CustID'];
+            $Song = $row['SongID'];
+            $Version = $row['Version'];
+            $Time = $row['Time'];
+            $Money = $row['Money'];
+            echo "<h1><tr><td><input type='radio' name='Pqueue' value='$Song' '$id'>$id &nbsp</td><td>$Song &nbsp</td><td>$Version &nbsp </td><td>$Time &nbsp</td><td>$Money &nbsp </td></tr></h1>";
+        }
+        echo "</tbody></table>";
 
-        // get first person from the queue and add it into where clause under
-        $queue2 = $pdo->prepare("SELECT Name,Time from Customers,PriorityQueues where Customers.CustID = PriorityQueues.CustID order by Time");
+        $queue2 = $pdo->prepare("SELECT * from Customers,PriorityQueues where Customers.CustID = PriorityQueues.CustID order by Time");
         $queue2->execute();
         $result = $queue2 -> fetch(PDO :: FETCH_ASSOC);
-        if($result)
+
+        echo "<form method = \"post\"><button type = \"submit\" name = \"Delete\" value = \"clicked\">SING</button></form>";
+        if(isset($_GET['Pqueue'])&& $_GET['Delete'] === 'clicked')
         {
             echo "</br>";
             echo "<h1>";
@@ -35,7 +59,6 @@
             echo "</br>";
             echo "</h1>";
         }
-
         ?>
       
 
