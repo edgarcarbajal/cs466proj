67911771f6ede4db4a677aec26bb066849aeec37
Yonas Chemeda <z1899807@turing.cs.niu.edu>
2023-04-27 16:18:09 -0500

    made edits

--- php_files/showTables.php
+++ php_files/showTables.php
@@ -132,4 +132,49 @@ function createDropdownNorm($arr, $elem_name)
  
     return true;
 }
+function createTblRadio($result_set)
+{
+    if($result_set) 
+    {
+        echo "<table border=1 cellspacing=1>\n";
+        $i = 0;
+
+        while($row = $result_set->fetch(PDO::FETCH_ASSOC))
+        {
+            #print headers of tables on top row
+            if($i == 0)
+            {
+                echo "<tr>\n";
+                foreach($row as $key => $item) { echo "<th>$key</th>"; }
+                echo "</tr>\n";
+            }
+            echo "<tr>\n";
+            $j = 0;
+            foreach($row as $key => $item) 
+            {
+                if($j == 0)
+                {
+                   $ver = $row['Version'];
+                   $sngid = $row['SongID'];
+                    echo "<td>"; 
+                    echo "<input type=\"radio\" id=\"song_$i\" name=\"PQUEUE\" value=\"$item,$ver,$sngid\">";
+                    echo "<label for=\"song_$i\">$item</label></td>";
+                }
+                else{ echo "<td>$item</td>"; }  
+                $j++;
+            }
+            echo "</tr>\n";
+            $i++;
+        }
+        echo "</table>\n"; 
+        if($i == 0)
+        {
+            echo "<p>Empty set! Nothing was shown.</p>\n";
+            return false; 
+        }
+    }
+    else{ echo "<p>Table not created - Failed to obtain a result set.</p>\n"; return false; }
+
+    return true;
+}
 ?>
