9f38bd9befd2c82b11e7163d77edc1b925ba6cbf
Mohamed Sharif <z1978180@turing.cs.niu.edu>
2023-05-01 14:55:42 -0500

    made the signin pretty

--- html_files/customer_signin.php
+++ html_files/customer_signin.php
@@ -7,7 +7,7 @@
     </head>
 
     <body>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark py-3">
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark py-3">
             <div class="container-fluid">
               <ul class="navbar-nav">
               <li class="nav-item">
@@ -25,50 +25,70 @@
               </ul>
             </div>
         </nav>
+
         <h1>Sign In, or create a new Customer ID</h1>
 
         <p>
             In order to select a song, you must first sign in by providing your Customer ID below.
             If you do not have a Customer ID, you can create one below.
         </p>
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
+                        <form action="https://students.cs.niu.edu/~z1895668/cs466proj/html_files/customerview.php" method="GET">
+                            <h3 class="mb-5">Sign in</h3>
 
-        <form action="https://students.cs.niu.edu/~z1895668/cs466proj/html_files/customerview.php" method="GET">
+                            <div class="form-outline mb-4">
+                            <input type="text" id="custid" name="custidtf" required class="form-control form-control-lg" />
+                            <label class="form-label" for="custid">Customer ID:</label>
+                            </div>
 
-            <h2>Sign in using Customer ID</h2>
-            <p>
-                <label for="custid">Customer ID:</label><br>
-                <input type="text" id="custid" name="custidtf" required><br>
-            </p>
-            <input type="submit" value="Sign In">
+
+                            <button class="btn btn-primary btn-lg btn-block" type="submit" value="Sign In">Login</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
+
+                            <h3 class="mb-5">Register</h3>
+                            <form action="https://students.cs.niu.edu/~z1895668/cs466proj/html_files/cust_cont.php" method="GET">
+
+                            <div class="form-outline mb-4">
+                            <input type="text" id="name" name="nametf" required class="form-control form-control-lg" />
+                            <label class="form-label" for="name">Name</label>
+                            </div>
+
+                            <div class="form-outline mb-4">
+                            <input type="text" id="email" name="emailtf" required class="form-control form-control-lg" />
+                            <label class="form-label" for="email">Email</label>
+                            </div>
+
+                            <div class="form-outline mb-4">
+                            <input type="text" id="phone" name="phonetf" required class="form-control form-control-lg" />
+                            <label class="form-label" for="phone">Phone Number (No Dashes):</label>
+                            </div>
+
+                            <button class="btn btn-primary btn-lg btn-block" type="submit" value="Create ID">Register</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
         
-        </form>
-
-            <br><br>
+        <?php
+            include "../php_files/PDOStartup.php";
         
-        <form action="https://students.cs.niu.edu/~z1895668/cs466proj/html_files/cust_cont.php" method="GET">
-            <h2>Create Customer ID</h2>
-            <p>
-                <label for="name">Name:</label><br>
-                <input type="text" id="name" name="nametf" required><br>
+        
+        ?>
 
-                <label for="email">Email:</label><br>
-                <input type="text" id="email" name="emailtf" required><br>
-
-                <label for="phone">Phone Number (no dashes):</label><br>
-                <input type="text" id="phone" name="phonetf" required><br>
-            </p>
-            <input type="submit" value="Create ID">
-
-        </form>
-            <?php
-                include "../php_files/PDOStartup.php";
-            
-            
-            ?>
-
-        <a href="startpage.html">
-            <input type="button" value="Go Back to Start Page">
-        </a>
         <footer class="text-center text-lg-start bg-dark">
             <div class="container d-flex justify-content-center py-5">
             </div>
