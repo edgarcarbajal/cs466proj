857111161fc39d374146169c3f783db67534cbee
Mohamed Sharif <z1978180@turing.cs.niu.edu>
2023-05-02 16:06:59 -0500

    Changed the bg to purple for all sites

--- html_files/queuesong.php
+++ html_files/queuesong.php
@@ -7,7 +7,7 @@
         <title>CSCI 466 Project - Karaoke Website: Queue Song</title>
     </head>
 
-    <body>
+    <body style="background-color: indigo;">
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark py-3">
             <div class="container-fluid">
               <ul class="navbar-nav">
